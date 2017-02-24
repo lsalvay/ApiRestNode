c6fa3f027416ffefe9cfb7ad18baf20719f8c437
Luis Pablo Salvay <lsalvay@gmail.com>
2017-02-24 12:18:09 -0300

    Commit from GitHub Desktop

---
+++ main.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const app = express()
+const port = process.env.Port || 3000
+
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+
+app.get('/api/products', (req, res) =>{
+	res.send(200, {products: []})
+	
+})
+
+app.get('/api/product/:productId', (req, res) =>{
+
+})	
+
+app.post('/api/product', (req, res) =>{
+	console.log(req.body)
+	res.status(200).send({message: 'El producto se ha recibido'})
+})
+
+app.put('/api/product/:productId', (req, res) =>{
+
+})
+
+app.delete('/api/product/:productId', (req, res) =>{
+
+})
+
+mongoose.connection('mongodb')
+
+app.listen(port, () => {
+	console.log(`API REST Corriendo en http://localhost:${port}`)
+})
