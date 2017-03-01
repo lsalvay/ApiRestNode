837546930f33421ae278bd0e62921ad738e25675
Luis Pablo Salvay <lsalvay@gmail.com>
2017-03-01 09:36:47 -0300

    added auth in routes

--- routes/index.js
+++ routes/index.js
@@ -10,22 +10,21 @@ const auth = require('../middlewares/auth')
 const api = express.Router()
 
 //Routes Alumns
-api.get('/patients', patientCtrl.getPatients)
-api.get('/patient/:patientId', patientCtrl.getPatient)	
-api.post('/patient', patientCtrl.savePatient)
-api.put('/patient/:patientId', patientCtrl.updatePatient)
-api.delete('/patient/:patientId', patientCtrl.deletePatient)
+api.get('/patients', auth.isAuth, patientCtrl.getPatients)
+api.get('/patient/:patientId' auth.isAuth,, patientCtrl.getPatient)	
+api.post('/patient', auth.isAuth, patientCtrl.savePatient)
+api.put('/patient/:patientId', auth.isAuth, patientCtrl.updatePatient)
+api.delete('/patient/:patientId', auth.isAuth, patientCtrl.deletePatient)
 
 //Routes Appointment
-api.get('/appointments', appointmentCtrl.getAppointments)
-api.get('/appointment/:appointmentId', appointmentCtrl.getAppointment)	
-api.post('/appointment', appointmentCtrl.saveAppointment)
-api.put('/appointment/:appointmentId', appointmentCtrl.updateAppointment)
-api.delete('/appointment/:appointmentId', appointmentCtrl.deleteAppointment)
+api.get('/appointments', auth.isAuth, appointmentCtrl.getAppointments)
+api.get('/appointment/:appointmentId', auth.isAuth, appointmentCtrl.getAppointment)	
+api.post('/appointment', auth.isAuth, appointmentCtrl.saveAppointment)
+api.put('/appointment/:appointmentId', auth.isAuth, appointmentCtrl.updateAppointment)
+api.delete('/appointment/:appointmentId', auth.isAuth, appointmentCtrl.deleteAppointment)
 
 //Routes Auth
 api.post('/signUp', authCtrl.signUp)
 api.post('/signIn', authCtrl.signIn)
-api.get('/private', auth.isAuth, patientCtrl.getPatients)
 
 module.exports = api
